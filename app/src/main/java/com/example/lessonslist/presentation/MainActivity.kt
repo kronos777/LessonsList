368514a7b7d89bb7a722f4d7899a8db3646a57c5
Kronos777 <ziswp1@gmail.com>
2022-03-22 22:55:20 +0300

    add drawer menu

--- app/src/main/java/com/example/lessonslist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lessonslist/presentation/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lessonslist.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -12,6 +16,10 @@ import com.example.lessonslist.databinding.ActivityMainBinding
 import com.example.lessonslist.presentation.student.StudentItemActivity
 import com.example.lessonslist.presentation.student.StudentItemFragment
 import com.example.lessonslist.presentation.student.StudentListAdapter
+import com.google.android.material.internal.ContextUtils.getActivity
+import com.google.android.material.internal.NavigationMenuItemView
+import com.google.android.material.navigation.NavigationBarItemView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity(), StudentItemFragment.OnEditingFinishedListener {
@@ -21,8 +29,12 @@ class MainActivity : AppCompatActivity(), StudentItemFragment.OnEditingFinishedL
     private lateinit var shopListAdapter: StudentListAdapter
     private lateinit var binding: ActivityMainBinding
 
+    lateinit var toggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRecyclerView()
@@ -38,7 +50,34 @@ class MainActivity : AppCompatActivity(), StudentItemFragment.OnEditingFinishedL
                 launchFragment(StudentItemFragment.newInstanceAddItem())
             }
         }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.navView)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.muItem1 -> Toast.makeText(this,"Text1!",Toast.LENGTH_SHORT).show()
+                R.id.muItem2 -> Toast.makeText(this,"Text2!",Toast.LENGTH_SHORT).show()
+                R.id.muItem3 -> Toast.makeText(this,"Text3!",Toast.LENGTH_SHORT).show()
+                R.id.muItem4 -> Toast.makeText(this,"Text4 !",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
     }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onEditingFinished() {
         Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/lessonslist/presentation/student/StudentItemFragment.kt
+++ app/src/main/java/com/example/lessonslist/presentation/student/StudentItemFragment.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.lessonslist.databinding.FragmentStudentItemBinding
